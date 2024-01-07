1cba39dd5bd7e29f69082633e4eb815e00c64bad
Anderson22-dev <andersonsilvasouza714@gmail.com>
2024-01-06 23:42:49 -0300

    first commit

---
+++ projeto-react/src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from "./post"
+import './styles.css'
+
+export function App() {
+
+  return (
+    <div>
+        <Post author="Anderson" content="Primeiro post!"  />
+        
+    </div>
+      
+    
+  )
+}
+
+
